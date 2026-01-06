09e11395d6dc60ef16f5d241b4ab2330f89b12ab
KhangNPSE194017 <npkhang2005@gmail.com>
2026-01-07 00:44:35 +0700

    New Commit

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,9 +2,7 @@ const Card = () => {
   return (
     <div className="w-70 h-80 bg-gray-100 rounded flex flex-col justify-start items-center p-5 hover:cursor-pointer hover:scale-105 transition ease-out duration-200 border border-gray-200">
       <div className="w-full h-3/4 bg-gray-300" />
-      <text className="pt-3 font-thin w-full text-start">
-        Image description
-      </text>
+      <text className="pt-3 font-thin w-full text-start">Image</text>
     </div>
   );
 };
